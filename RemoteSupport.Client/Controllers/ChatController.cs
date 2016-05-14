921d6f8f31b5756baf0275e07e76857befc2f545
Oleg Pylypchak <liole.plo@gmail.com>
2016-05-14 18:59:37 +0300

    Chat template

---
+++ RemoteSupport.Client/Controllers/ChatController.cs
@@ -0,0 +1,34 @@
+﻿using RemoteSupport.Client.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using System.Windows.Forms;
+
+namespace RemoteSupport.Client.Controllers
+{
+	public class ChatController
+	{
+		private IChatForm chatForm;
+
+		public ChatController(IChatForm chatForm)
+		{
+			this.chatForm = chatForm;
+
+			Program.ConnectionController.CommandHub.On("SendMessage", (Action<string, string>)this.ReceiveMessage);
+		}
+
+		public void ReceiveMessage(string user, string message)
+		{
+			MessageBox.Show(user + ":" + message);
+			//TODO: show message on form
+		}
+
+		public void SendMessage(string message)
+		{
+			Program.ConnectionController.CommandHub.Invoke("SendMessage", message);
+		}
+	}
+}
---
+++ RemoteSupport.Client/View/ChatForm.cs
@@ -0,0 +1,39 @@
+﻿using RemoteSupport.Client.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteSupport.Client.View
+{
+	public partial class ChatForm : Form, IChatForm
+	{
+		public ChatController chat;
+
+		public ChatForm()
+		{
+			InitializeComponent();
+			chat = new ChatController(this);
+		}
+
+		public void Clear()
+		{
+			//TODO: clear chat window
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			chat.SendMessage("Hello world!");
+		}
+	}
+
+	public interface IChatForm
+	{
+
+	}
+}
